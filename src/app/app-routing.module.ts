c1ce2754b811ead861697c6a3da838aba4b7dd81
Milena8903 <75911518+Milena8903@users.noreply.github.com>
2022-02-25 19:07:14 -0500

    firstCommit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,37 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import {AgendaComponent} from './agenda/agenda.component';
+import {CitaMedicaComponent} from './cita-medica/cita-medica.component';
+import {ConsejosComponent} from './consejos/consejos.component';
+import {PoliticasComponent} from './politicas/politicas.component';
 
-const routes: Routes = [];
+// por defecto a penas el usuario abra la pagina
+// se vaya a welcome
+const routes: Routes = [
+  {
+    path: "",
+    redirectTo: "/welcome",
+    pathMatch: "full",
+  },
+  {
+    path:'agenda',
+    component: AgendaComponent
+  },
+  {
+    path:'cita-medica',
+    component: CitaMedicaComponent
+  },
+  {
+    path:'consejos',
+    component: ConsejosComponent
+  },
+  {
+    path:'politicas',
+    component: PoliticasComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { LoginModule } from './login/login.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PoliticasComponent } from './politicas/politicas.component';
+import { CitaMedicaComponent } from './cita-medica/cita-medica.component';
+import { ConsejosComponent } from './consejos/consejos.component';
+import { AgendaComponent } from './agenda/agenda.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent, FooterComponent
+    AppComponent, FooterComponent, HeaderComponent, PoliticasComponent, CitaMedicaComponent, ConsejosComponent, AgendaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LoginModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
